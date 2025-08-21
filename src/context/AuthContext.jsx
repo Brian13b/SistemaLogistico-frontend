4d578ef10bb4c6cee70b43a127e3816995d79e99
Brian Battauz <122492710+Brian13b@users.noreply.github.com>
2025-08-21 20:49:21 -0300

    Update AuthContext.jsx

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useState, useContext, useEffect } from 'react';
-import { authService } from '../services/AuthService.jsx';
+import { authService } from '../services/authService';
 import { jwtDecode } from 'jwt-decode';
 
 const AuthContext = createContext(null);
