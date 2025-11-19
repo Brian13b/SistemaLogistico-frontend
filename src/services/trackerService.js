e7be00f00706e2f4d258e4ba62e4bc601e4a7707
Brian Battauz <brian.nbattauz@gmail.com>
2025-11-19 20:28:45 -0300

    Cambios en los services de tracker

--- src/services/trackerService.js
+++ src/services/trackerService.js
@@ -24,21 +24,21 @@ import api from './api';
 
 export const trackerService = {
     obtenerUbicacionActual: (dispositivoId) => 
-        api.get(`/tracker/tracker/dispositivo/${dispositivoId}/actual`),
+        api.get(`/tracker/dispositivo/${dispositivoId}/actual`),
 
     obtenerHistorialUbicaciones: (dispositivoId, fechaInicio, fechaFin) => 
-        api.get(`/tracker/tracker/dispositivo/${dispositivoId}/historial`, {
+        api.get(`/tracker/dispositivo/${dispositivoId}/historial`, {
             params: {
                 fechaInicio,
                 fechaFin
             }
         }),
 
-    obtenerVehiculos: () => api.get('/tracker/vehiculos'),
+    obtenerVehiculos: () => api.get('/vehiculos'),
     
-    obtenerVehiculoPorId: (vehiculoId) => api.get(`/tracker/vehiculos/${vehiculoId}`),
+    obtenerVehiculoPorId: (vehiculoId) => api.get(`/vehiculos/${vehiculoId}`),
 
     obtenerDispositivos: () => api.get('/dispositivos'),
     
-    obtenerDispositivoPorId: (dispositivoId) => api.get(`/tracker/dispositivos/${dispositivoId}`),
+    obtenerDispositivoPorId: (dispositivoId) => api.get(`/dispositivos/${dispositivoId}`),
 };
